37c1f09551b6120335ba2075a144401a7be79e3b
lfgut <luisfranciscogutierrez@gmail.com>
2022-07-04 19:02:05 +0200

    Con LonjaApp

---
+++ cursoJava/src/lonjaApp/TestearLonja.java
@@ -0,0 +1,21 @@
+package lonjaApp;
+
+import lonjaApp.*;
+import lonjaApp.apoyo.Lonja;
+
+//import lonja.apoyo.Lonja;
+
+public class TestearLonja {
+
+	public static void main(String[] args) {
+		
+		Lonja l1 = new Lonja();
+		l1.pintar();
+		
+		
+		
+
+	}
+
+
+}
---
+++ cursoJava/src/lonjaApp/apoyo/Lonja.java
@@ -0,0 +1,15 @@
+package lonjaApp.apoyo;
+
+import java.util.List;
+
+public class Lonja{
+
+	public void pintar() {
+		this.toString();
+		Object obj;
+
+	}
+
+	
+	
+}
