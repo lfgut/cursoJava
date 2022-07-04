5ccf47ed8183e0718021b6266aa9a97bbcab7abc
lfgut <luisfranciscogutierrez@gmail.com>
2022-07-04 20:04:13 +0200

    Primer workflow

--- cursoJava/src/lonjaApp/TestearLonja.java
+++ cursoJava/src/lonjaApp/TestearLonja.java
@@ -1,20 +1,60 @@
 package lonjaApp;
 
 import lonjaApp.*;
-import lonjaApp.apoyo.Lonja;
+import lonjaApp.apoyo.*;
 
 //import lonja.apoyo.Lonja;
 
 public class TestearLonja {
 
 	public static void main(String[] args) {
-		
+		new TestearLonja();
+	}
+	
+	private TestearLonja() {
 		Lonja l1 = new Lonja();
 		l1.pintar();
 		
+		String vamos ="CLPVF";
+		int momento = 0;
+		char ahora;
 		
-		
+		do {
+			ahora = vamos.charAt(momento);
+			switch (ahora) {
+			case 'C': {
+		// LLegan los barcos con cajas de pescado
+				l1.cargar();
+				l1.pintar();
+				break;
+			}
+			case 'L': {
+				l1.limpiar();
+				l1.pintar();
+				break;
+			}
+			case 'P': {
+				l1.pesar();
+				l1.pintar();
+				break;
+			}
+			case 'V': {
+				l1.valorar();
+				l1.pintar();
+				break;
+			}
+			case 'F': {
+				l1.vaciar();
+				l1.pintar();
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("No se que quieres hacer: " + ahora);
+			}
+			momento++;
+		} while ( (ahora != 'F') && (momento < vamos.length()) );
 
+		System.out.println("Hemos terminado");
 	}
 
 
--- cursoJava/src/lonjaApp/apoyo/Lonja.java
+++ cursoJava/src/lonjaApp/apoyo/Lonja.java
@@ -2,14 +2,55 @@ package lonjaApp.apoyo;
 
 import java.util.List;
 
+/*
+import lonjaApp.Almeja;
+import lonjaApp.Boqueron;
+import lonjaApp.Dorada;
+import lonjaApp.Gamba;
+import lonjaApp.Merluza;
+import lonjaApp.Sardina;
+*/
 public class Lonja{
 
+//		Testeamos
+	public void cargar() {
+		System.out.println("Cargando...");
+		/*
+		cargar( new Merluza() );
+		cargar( new Gamba() );
+		cargar( new Almeja() );
+		cargar( new Dorada() );
+		cargar( new Sardina() );
+		cargar( new Boqueron() );
+		cargar( new Gamba() );
+		cargar( new Panga() );
+		*/
+	}
+	
 	public void pintar() {
-		this.toString();
-		Object obj;
-
+		System.out.println(this);
 	}
 
-	
-	
+	public void limpiar() {
+		System.out.println("Limpiando...");
+	}
+
+	public void pesar() {
+		System.out.println("Pesando...");
+	}
+
+	public void valorar() {
+		System.out.println("Valorando...");
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Soy una lonja";
+	}
+
+	public void vaciar() {
+		System.out.println("Vaciando...");
+	}
+
 }
