dc42f4e6f6f72745b83e82f1aca277f3fe3500d8
lfgut <luisfranciscogutierrez@gmail.com>
2022-07-08 20:32:25 +0200

    Practica de coches con maletero

---
+++ cursoJava/src/carrerasdecoches/apoyo/CocheConMaletero.java
@@ -0,0 +1,15 @@
+package carrerasdecoches.apoyo;
+
+public abstract class CocheConMaletero  {
+	private String maletero;
+	
+	private String getMaletero() {
+		return maletero;
+	}
+
+	private void setMaletero(String maletero) {
+		this.maletero = maletero;
+	}
+
+
+}
