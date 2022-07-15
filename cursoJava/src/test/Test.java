e962c4106e0401faf2f1cf426752ddbd5881abc9
lfgut <luisfranciscogutierrez@gmail.com>
2022-07-15 20:30:58 +0200

    Tests

---
+++ cursoJava/src/test/Test.java
@@ -0,0 +1,17 @@
+package test;
+
+import libreta.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Object obj = new Integer(3);
+
+		Integer num = (Integer) obj;
+		
+		System.out.println(num);
+	}
+	
+
+}
